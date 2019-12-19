a38bdc45fd1eb3e605a9093376a52803717ea539
suraj130499 <58870929+suraj130499@users.noreply.github.com>
2019-12-19 10:46:49 +0530

    Add files via upload

---
+++ divibile.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a%3==0)
+	{
+		printf("FIZZ");
+		
+	}
+	 if(a%5==0)
+	{
+		printf("bizz");
+		
+	}
+      else
+	{
+		printf("no");
+	}
+}s
